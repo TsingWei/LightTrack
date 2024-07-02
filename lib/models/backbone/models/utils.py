e34a557208901eb7ce9b55d01c5e2badef35003d
TsingWei <461626239@qq.com>
2024-07-02 06:03:01 +0000

    modded for test.

--- lib/models/backbone/models/utils.py
+++ lib/models/backbone/models/utils.py
@@ -5,20 +5,11 @@ import torch.nn.functional as F
 import numpy as np
 
 from typing import Tuple, Optional, List
-from torch._six import container_abcs
+# from torch._six import container_abcs
 from itertools import repeat
 
 
-def _ntuple(n):
-    def parse(x):
-        if isinstance(x, container_abcs.Iterable):
-            return x
-        return tuple(repeat(x, n))
 
-    return
-
-
-tup_pair = _ntuple(2)
 
 
 def get_same_padding(x: int, k: int, s: int, d: int):
--- tracking/FLOPs_Params.py
+++ tracking/FLOPs_Params.py
@@ -1,47 +1,70 @@
 import _init_paths
 from lib.models.models import LightTrackM_Speed
-
-from thop import profile
-from thop.utils import clever_format
 import torch
-# from efficientnet_pytorch.utils import Conv2dDynamicSamePadding
-# from efficientnet_pytorch.utils import Conv2dStaticSamePadding
-# from efficientnet_pytorch.utils import MemoryEfficientSwish
-# from thop.vision.basic_hooks import count_convNd, zero_ops
+import time
+import numpy as np
+import onnxruntime as ort
+from onnxconverter_common import float16
+import onnx
+from onnxsim import simplify as simplify_func
 
 if __name__ == "__main__":
-    # Compute the Flops and Params of our LightTrack-Mobile model
-    # build the searched model
-    path_name = 'back_04502514044521042540+cls_211000022+reg_100000111_ops_32'  # LightTrack-Mobile model
+    # test the running speed
+    path_name = 'back_04502514044521042540+cls_211000022+reg_100000111_ops_32'  # our 530M model
+    use_gpu = False
+    # torch.cuda.set_device(0)
     model = LightTrackM_Speed(path_name=path_name)
-    print(model)
-    backbone = model.features
-    head = model.head
-
     x = torch.randn(1, 3, 256, 256)
     zf = torch.randn(1, 96, 8, 8)
 
-    inp = {'cls': torch.randn(1, 128, 16, 16), 'reg': torch.randn(1, 128, 16, 16)}
-    oup = model(x, zf)
+    dtype = np.float16
+    inputs=( x,zf)
+    inputs_onnx = {'zf':  np.array(zf.cpu(), dtype=dtype),
+                   'x': np.array(x.cpu(), dtype=dtype),
+                   }
+    # output_onnx_name = 'test_net.onnx'
+    # torch.onnx.export(model, 
+    #     inputs,
+    #     output_onnx_name, 
+    #     input_names=[  "x","zf",], 
+    #     output_names=["output"],
+    #     opset_version=11,
+    #     export_params=True,
+    #     # verbose=True,
+    #     # dynamic_axes={'input':{0:'batch', 2:'h', 3:'w'}, 'output':{0:'batch', 2:'h2', 3:'w2'}} 
+    # )
+    # providers = ['CPUExecutionProvider']
+    # onnx_model = onnx.load("test_net.onnx")
+    
+    # model_fp16 = float16.convert_float_to_float16(onnx_model)
+    # model_fp16, success = simplify_func(onnx_model)
+    # model_fp16 = float16.convert_float_to_float16(model_fp16)
 
-    # custom_ops = {
-    #     Conv2dDynamicSamePadding: count_convNd,
-    #     Conv2dStaticSamePadding: count_convNd,
-    #     MemoryEfficientSwish: zero_ops,
-    # }
-    # compute FLOPs and Params
-    # the whole model
-    macs, params = profile(model, inputs=(x, zf), custom_ops=None, verbose=False)
-    macs, params = clever_format([macs, params], "%.3f")
-    print('overall macs is ', macs)
-    print('overall params is ', params)
-    # backbone
-    macs, params = profile(backbone, inputs=(x,), custom_ops=None, verbose=False)
-    macs, params = clever_format([macs, params], "%.3f")
-    print('backbone macs is ', macs)
-    print('backbone params is ', params)
-    # head
-    macs, params = profile(head, inputs=(inp,), verbose=False)
-    macs, params = clever_format([macs, params], "%.3f")
-    print('head macs is ', macs)
-    print('head params is ', params)
+    # onnx.save(model_fp16, "test_net_fp16.onnx")
+    # ort_session = ort.InferenceSession("test_net_fp16.onnx", providers=providers)
+    # # output = ort_session.run(output_names=['output'],
+    # #                          	input_feed=inputs_onnx,
+    # #                             )
+
+    if use_gpu:
+        model = model.cuda()
+        x = x.cuda()
+        zf = zf.cuda()
+    # oup = model(x, zf)
+
+    T_w = 100  # warmup
+    T_t = 500  # test
+    with torch.no_grad():
+        for i in range(T_w):
+            oup = model(x, zf)
+            # output = ort_session.run(output_names=['output'],
+            #                  	input_feed=inputs_onnx,
+            #                     )
+        t_s = time.time()
+        for i in range(T_t):
+            oup = model(x, zf)
+            # output = ort_session.run(output_names=['output'],
+            #                  	input_feed=inputs_onnx,
+            #                     )
+        t_e = time.time()
+        print('speed: %.2f FPS' % (T_t / (t_e - t_s)))
